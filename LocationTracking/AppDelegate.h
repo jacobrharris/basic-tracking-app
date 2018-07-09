b846b8fb2d3eb9ad2e2d2f476cbfae38b0158687
Jacob Harris <jacob@jacobharris.org>
2018-07-09 16:39:18 -0400

    Initial Commit

---
+++ LocationTracking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationTracking
+//
+//  Created by Jacob Harris on 7/9/18.
+//  Copyright © 2018 Sneeze & Cookie, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationTracking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationTracking
+//
+//  Created by Jacob Harris on 7/9/18.
+//  Copyright © 2018 Sneeze & Cookie, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
