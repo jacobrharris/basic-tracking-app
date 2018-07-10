775082b11ac67a5292a276a08334184a48db0ca0
Jacob Harris <jacob@jacobharris.org>
2018-07-10 10:27:24 -0400

    saving coordinates locally

--- LocationTracking/ViewController.m
+++ LocationTracking/ViewController.m
@@ -17,18 +17,53 @@
 
 @end
 
+NSString * const CoordinatesKey = @"CoordinatesKey";
+NSString * const LatitudeKey = @"LatitudeKey";
+NSString * const LongitudeKey = @"LongitudeKey";
+NSString * const TitleKey = @"TitleKey";
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Set up location manager
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.allowsBackgroundLocationUpdates = YES;
     self.locationManager.showsBackgroundLocationIndicator = YES;
 
+    // Set up map view
     self.mapView.showsUserLocation = YES;
     self.mapView.delegate = self;
+    
+    // Retrieve any previously-saved coordinates and add them to the map view
+    NSArray *savedCoordinates = [[NSUserDefaults standardUserDefaults] objectForKey:CoordinatesKey];
+    for (NSDictionary *coordinateDict in savedCoordinates) {
+        MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+        point.title = [coordinateDict objectForKey:TitleKey];
+        point.coordinate = CLLocationCoordinate2DMake([[coordinateDict objectForKey:LatitudeKey] doubleValue],
+                                                      [[coordinateDict objectForKey:LongitudeKey] doubleValue]);
+        [self.mapView addAnnotation:point];
+    }
+}
+
+- (void)saveCoordinate:(CLLocationCoordinate2D)coordinate withTitle:(NSString *)title {
+    // Retrieve any previously-saved coordinates
+    NSArray *savedCoordinates = [[NSUserDefaults standardUserDefaults] objectForKey:CoordinatesKey];
+    NSMutableArray *updatedCoordinates = [NSMutableArray array];
+    if (savedCoordinates) {
+        updatedCoordinates = [savedCoordinates mutableCopy];
+    }
+    
+    // Append the current coordinate to the saved coordinates
+    NSDictionary *coordinateDict = @{LatitudeKey:@(coordinate.latitude),
+                                     LongitudeKey:@(coordinate.longitude),
+                                     TitleKey:title};
+    [updatedCoordinates addObject:coordinateDict];
+    
+    // Save the coordinates
+    [[NSUserDefaults standardUserDefaults] setObject:updatedCoordinates forKey:CoordinatesKey];
 }
 
 - (IBAction)didTapLocationButton:(UIButton *)sender {
@@ -44,17 +79,25 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
     NSLog(@"%@", locations);
-    for (CLLocation *location in locations) {
-        MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
-        point.coordinate = location.coordinate;
+
+    // Create an annotation when a location is created
+    CLLocation *lastLocation = [locations lastObject];
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    point.coordinate = lastLocation.coordinate;
+    
+    // Reverse-geocode the location to get the address
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:lastLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        // Add the address to the annotation
+        CLPlacemark *placemark = [placemarks firstObject];
+        point.title = placemark.name;
         
-        CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-        [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-            CLPlacemark *placemark = [placemarks firstObject];
-            point.title = placemark.name;
-            [self.mapView addAnnotation:point];
-        }];
-    }
+        // Add the annotation to the map
+        [self.mapView addAnnotation:point];
+        
+        // Save the coordinate and title to NSUserDefaults
+        [self saveCoordinate:lastLocation.coordinate withTitle:point.title];
+    }];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
@@ -64,12 +107,12 @@
 #pragma mark - MKMapViewDelegate
 
 - (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray<MKAnnotationView *> *)views {
-    for (MKAnnotationView *view in views) {
-        CLLocationCoordinate2D coord = view.annotation.coordinate;
-        MKCoordinateSpan span = MKCoordinateSpanMake(0.02, 0.02);
-        MKCoordinateRegion region = MKCoordinateRegionMake(coord, span);
-        [mapView setRegion:region animated:YES];
-    }
+    // Set the map region when an annotation is added
+    MKAnnotationView *view = [views lastObject];
+    CLLocationCoordinate2D coord = view.annotation.coordinate;
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.02, 0.02);
+    MKCoordinateRegion region = MKCoordinateRegionMake(coord, span);
+    [mapView setRegion:region animated:YES];
 }
 
 @end
